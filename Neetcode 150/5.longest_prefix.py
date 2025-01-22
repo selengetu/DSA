28306e1641255346bc8d98db4a7c81f8a8cbeea1
selengetu <Selengetu@gmail.com>
2025-01-22 13:26:24 -0600

    prefix

--- Neetcode 150/5.longest_prefix.py
+++ Neetcode 150/5.longest_prefix.py
@@ -15,3 +15,16 @@ class Solution:
                     return ""
         
         return prefix
+
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i ==len(s ) or s[i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
+        return res  
+                
